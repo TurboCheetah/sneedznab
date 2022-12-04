4de893d1613fe38daca614f69c84c61882c33b2a
Turbo <dev@turbo.ooo>
2022-12-04 13:20:38 -0500

    feat: Add ruTracker support

---
+++ src/utils/Rutracker.ts
@@ -0,0 +1,86 @@
+import { rutrackerUrl } from '#/constants'
+import { IRutrackerData } from '#interfaces/rutracker'
+import { IProvider } from '#interfaces/provider'
+import { ITorrentRelease, IUsenetRelease } from '#interfaces/releases'
+import { ISneedexRelease } from '#interfaces/sneedex'
+import { app } from '#/index'
+import { debugLog } from '#utils/debugLog'
+
+export class Rutracker implements IProvider {
+  constructor() {
+    this.name = 'ruTracker'
+  }
+
+  // provider specific fetch function to retrieve raw data
+  private async fetch(query: string): Promise<IToshoData[]> {
+    debugLog(`${this.name} (cache): ${this.name}_${query}`)
+    const cachedData = await app.cache.get(`${this.name}_${query}`)
+    if (cachedData) {
+      debugLog(`${this.name} (cache): Cache hit: ${this.name}_${query}`)
+      return cachedData as IToshoData[]
+    }
+    debugLog(`${this.name} (cache): Cache miss: ${this.name}_${query}`)
+
+    const searchURL = `${rutrackerUrl}/get_tor_topic_data?by=topic_id&val=${query}`
+
+    debugLog(`${this.name} (fetch): ${query}`)
+    debugLog(`${this.name} (fetch): Fetching data from ${searchURL}`)
+
+    const data = await fetch(searchURL).then(res => {
+      if (!res.ok) throw new Error(res.statusText)
+      return res.json()
+    })
+
+    debugLog(
+      `${this.name} (fetch): Fetched data, caching ${this.name}_${query}`
+    )
+    await app.cache.set(`${this.name}_${query}`, data)
+
+    return data as IRutrackerData[]
+  }
+
+  // get function to standardize the returned data to make things easier to work with and plug-and-play
+  public async get(
+    anime: string,
+    sneedexData: ISneedexRelease
+  ): Promise<ITorrentRelease[]> {
+    // first check if there even is an rutracker link in sneedexData.best_links and sneedexData.alt_links
+    const bestReleaseLinks = sneedexData.best_links.length
+      ? sneedexData.best_links.split(' ')
+      : sneedexData.alt_links.split(' ')
+
+    const threadLink = bestReleaseLinks.find((url: string) =>
+      url.includes('https://rutracker.org/forum/viewtopic.php?t=')
+    )
+    if (!threadLink) return null
+
+    // extract the thread ID from the link
+    // https://rutracker.org/forum/viewtopic.php?t=4035529 where 4035529 is the thread ID
+    const threadID = threadLink.split('t=')[1]
+
+    // call the ruTracker API
+    const data = await this.fetch(threadID)
+
+    return [
+      {
+        title: data.result[threadID].topic_title,
+        link: `https://rutracker.org/forum/viewtopic.php?t=${threadID}`,
+        url: `magnet:?xt=urn:btih:${
+          data.result[threadID].info_hash
+        }&tr=${encodeURIComponent(
+          `http://bt3.t-ru.org/ann?magnet&dn=${data.result[
+            threadID
+          ].topic_title.slice(0, 150)}`
+        )}`, // not sure why they only include the first 150 chars of the title post but ok
+        seeders: data.result[threadID].seeders,
+        leechers: 0, // API doesn't report this
+        infohash: data.result[threadID].info_hash,
+        size: data.result[threadID].size,
+        files: null,
+        timestamp: null,
+        grabs: data.result[threadID].dl_count,
+        type: 'torrent'
+      }
+    ] as ITorrentRelease[]
+  }
+}
