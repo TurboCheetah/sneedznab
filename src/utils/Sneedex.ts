e74efa8440748539cad945ddec428802863f422d
Turbo <dev@turbo.ooo>
2022-12-04 21:15:52 -0500

    feat: use the new public Sneedex endpoint
    feat: use the new public Sneedex endpoint

--- src/utils/Sneedex.ts
+++ src/utils/Sneedex.ts
@@ -2,7 +2,7 @@ import { sneedexUrl } from '#/constants'
 import { ISneedexData } from '#interfaces/sneedex'
 import { app } from '#/index'
 import { debugLog } from '#utils/debugLog'
-import { closest } from 'fastest-levenshtein'
+import { closest, distance } from 'fastest-levenshtein'
 
 export class Sneedex {
   public name: string
@@ -10,7 +10,7 @@ export class Sneedex {
     this.name = 'Sneedex'
   }
 
-  public async fetch(query: string): Promise<ISneedexData[]> {
+  public async fetch(query: string): Promise<ISneedexData> {
     debugLog(`${this.name}: ${query}`)
     debugLog(`${this.name} (cache): ${query}`)
     const cachedData = await app.cache.get(`${this.name}_${query}`)
@@ -35,14 +35,49 @@ export class Sneedex {
       release.alias = release.alias.replace(/\\n/gi, ' ').replace(/\n/gi, ' ')
       return release
     })
+    // find a release tile that contains the query
+    const release = data.find(release => {
+      const title = release.title.toLowerCase()
+      const alias = release.alias.toLowerCase()
+      return title === query.toLowerCase() || alias === query.toLowerCase()
+    })
+
+    // if we found a release, return it
+    // otherwise, find the closest match
+    // also gotta remember to parse the stringified releases
+    // also gotta strip out any weird stuff that isn't in the official title
+    // e.g Baki (2018) --> Baki
+    if (release) {
+      release.title = release.title
+        .replace(/\\n/gi, ' ')
+        .replace(/\n/gi, ' ')
+        .replace(/ \(\d{4}\)/gi, '')
+      release.releases = JSON.parse(release.releases)
+
+      debugLog(
+        `${this.name} (fetch): Fetched data, caching ${this.name}_${query}`
+      )
+      await app.cache.set(`${this.name}_${query}`, data as ISneedexData[])
+
+      return release as ISneedexData
+    }
 
     // find the object whose title or alias matches the query using fastest-levenshtein
     const closestMatch = closest(query, [
       ...data.map(release => release.title),
       ...data.map(release => release.alias).filter(alias => alias)
     ])
+    // get the distance between the query and the closest match
+    const closestMatchDistance = distance(query, closestMatch)
 
-    // find the obhect whose title or alias matches the closest match
+    // to prevent false positives, only return the closest match if the distance is less than 5
+    if (closestMatchDistance > 5) {
+      debugLog(`${this.name} (fetch): No match found for ${query}, caching`)
+      await app.cache.set(`${this.name}_${query}`, null)
+      return null
+    }
+
+    // find the object whose title or alias matches the closest match
     const closestMatchObject = data.find(
       release =>
         release.title === closestMatch ||
@@ -55,8 +90,8 @@ export class Sneedex {
     debugLog(
       `${this.name} (fetch): Fetched data, caching ${this.name}_${query}`
     )
-    await app.cache.set(`${this.name}_${query}`, data as ISneedexData[])
+    await app.cache.set(`${this.name}_${query}`, data as ISneedexData)
 
-    return match as ISneedexData[]
+    return match as ISneedexData
   }
 }
