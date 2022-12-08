37a502d00171f83ce0b969e0be76cf2007b14ab6
Turbo <dev@turbo.ooo>
2022-12-08 18:15:17 -0500

    fix: use strings instead of bools because Bun decided it would not longer parse environment variable types

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { RedisCache } from '#utils/Redis'
 import { SimpleCache } from '#utils/SimpleCache'
 
 export const app = new App(
-  process.env.REDIS_ENABLED
+  process.env.REDIS_ENABLED === 'true'
     ? new RedisCache(
         process.env.REDIS_URL,
         process.env.REDIS_TOKEN,
@@ -13,18 +13,18 @@ export const app = new App(
       )
     : new SimpleCache(),
   [
-    process.env.NYAA_ENABLED ? new Nyaa() : null,
+    process.env.NYAA_ENABLED === 'true' ? new Nyaa() : null,
     // AnimeTosho can be used instead of scraping Nyaa, but it's far less reliable
     // it's only useful if you want NZBs
-    process.env.ANIMETOSHO_ENABLED ? new AnimeTosho() : null,
+    process.env.ANIMETOSHO_ENABLED === 'true' ? new AnimeTosho() : null,
     // Only enable AnimeBytes if you have an account
-    process.env.ANIMEBYTES_ENABLED
+    process.env.ANIMEBYTES_ENABLED === 'true'
       ? new AnimeBytes(
           process.env.ANIMEBYTES_PASSKEY,
           process.env.ANIMEBYTES_USERNAME
         )
       : null,
-    process.env.RUTRACKER_ENABLED ? new Rutracker() : null
+    process.env.RUTRACKER_ENABLED === 'true' ? new Rutracker() : null
   ].filter(provider => provider !== null),
   [new ApiRoute()]
 )
